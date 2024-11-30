504dba47e1224ec0cb122074a695d2bf50dc2a15
Guilherme Burkner Marques <131628488+GuilhermeBMarques@users.noreply.github.com>
2024-11-29 22:05:34 -0300

    Update mercado.py

--- mercado.py
+++ mercado.py
@@ -16,10 +16,13 @@ class Mercado:
             
         self.cadastroProdutos = ttk.Button(janela, text="🏷️ Cadastro de produtos", command=self.adicionar_produto)
         self.cadastroProdutos.place(relx = 0.5, rely = 0.4, anchor = "center")
+        self.cadastroProdutos.configure(cursor='hand2')
         self.listaProdutos = ttk.Button(janela, text="📋 Exibição da lista de produtos cadastrados", command=self.exibir_produtos)
         self.listaProdutos.place(relx = 0.5, rely = 0.5, anchor = "center")
+        self.listaProdutos.configure(cursor='hand2')
         self.vendasProdutos = ttk.Button(janela, text="💸 Realização de vendas, com cálculo do total e atualização do estoque", command=self.realizar_venda)
         self.vendasProdutos.place(relx = 0.5, rely = 0.6, anchor = "center")
+        self.vendasProdutos.configure(cursor='hand2')
         
     def adicionar_produto(self):
         
@@ -38,10 +41,13 @@ class Mercado:
         self.quantidadeProduto = ttk.Entry(janela)
         self.quantidadeProduto.pack()
         
-        self.botao = ttk.Button(janela, text="Cadastrar", command=self.cadastroProduto)
-        self.botao.pack()
-        ttk.Button(self.janela, text="Voltar", command=self.Interface).pack(pady=5)
-        
+        self.cadastrar = ttk.Button(janela, text="Cadastrar", command=self.cadastroProduto)
+        self.cadastrar.pack()
+        self.cadastrar.configure(cursor='hand2')
+        self.voltar = ttk.Button(janela, text="Voltar", command=self.Interface)
+        self.voltar.pack()
+        self.voltar.configure(cursor='hand2')
+             
         self.cadastrado = tk.Label(janela, text="")
         self.cadastrado.pack()
         
@@ -75,7 +81,9 @@ class Mercado:
         for nome, preco, quantidade, codigo in self.dados:
             self.tree.insert("", tk.END, values=(nome, preco, quantidade, codigo))
         
-        ttk.Button(self.janela, text="Voltar", command=self.Interface).place(relx = 0.5, rely = 0.7, anchor = "center")
+        self.voltar = ttk.Button(janela, text="Voltar", command=self.Interface)
+        self.voltar.pack()
+        self.voltar.configure(cursor='hand2')
     
     def realizar_venda(self):
         
@@ -105,10 +113,13 @@ class Mercado:
         self.quantidadeProduto = ttk.Entry(janela)
         self.quantidadeProduto.pack(pady=10)
         
-        self.botao = ttk.Button(janela, text="Comprar", command=self.compra_realizada)
-        self.botao.pack()
+        self.comprar = ttk.Button(janela, text="Comprar", command=self.compra_realizada)
+        self.comprar.pack()
+        self.comprar.configure(cursor='hand2')
         
-        ttk.Button(self.janela, text="Voltar", command=self.Interface).pack()
+        self.voltar = ttk.Button(janela, text="Voltar", command=self.Interface)
+        self.voltar.pack()
+        self.voltar.configure(cursor='hand2')
         
         self.resultado = tk.Label(janela, text="")
         self.resultado.pack()
@@ -116,18 +127,22 @@ class Mercado:
     def compra_realizada(self):
         try:
             cb = int(self.codigoProduto.get())
-            quantidadeSocilicitada = int(self.quantidadeProduto.get())
+            quantidadeSolicitada = int(self.quantidadeProduto.get())
             
-            for index, (nome, preco, quantidade, codigo) in self.dados:
+            produto_encontrado = False
+            for i, (nome, preco, quantidade, codigo) in enumerate(self.dados):
             
                 if codigo == cb:
-                    if quantidadeSocilicitada > quantidade:
+                    produto_encontrado = True
+                    if quantidadeSolicitada > quantidade:
                         self.resultado.config(text=f"O numero é maior que a quantidade")
                     else:
-                        self.dados[index] = (nome, preco, quantidade - quantidadeSocilicitada, codigo)
+                        self.dados[i] = (nome, preco, quantidade - quantidadeSolicitada, codigo)
                         self.resultado.config(text=f"Compra realizada")
-                else:
-                    self.resultado.config(text=f"Esse codigo não existe!")
+                    break
+                
+            if not produto_encontrado:
+                self.resultado.config(text="Código do produto não encontrado!")
       
         except ValueError:
             self.resultado.config(text=f"Insira um numero!")
